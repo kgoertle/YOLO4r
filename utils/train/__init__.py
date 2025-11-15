806f610edaa9ff93044b07e94e6c95022aa790ed
kgoertle <startingupsterling@gmail.com>
2025-11-15 08:08:50 -0500

    beta-8.0
    Updated the top line of the helper util to define its place in the repository for quick reference.
    
    Signed-off-by: kgoertle <startingupsterling@gmail.com>

--- utils/train/__init__.py
+++ utils/train/__init__.py
@@ -1,3 +1,4 @@
+# utils/train/__init__.py
 from .config import get_args, get_training_paths
 from .io import ensure_yolo_yaml, ensure_weights, count_images, load_latest_metadata
 from .checkpoints import get_checkpoint_and_resume
